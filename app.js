5c55402f0458ccfe9eda89943c65130dddf6f423
Irina Savchenko <issavchenko.2011@gmail.com>
2018-12-26 22:18:33 +0200

    MongoDB usage

--- app.js
+++ app.js
@@ -1,84 +1,104 @@
-const express = require('express'); //принимает имя пакета и возвращает его(т.е. модуль express)
-const app = express(); //записываем его в переменную
+const express = require('express'); //принимает имя модуля и возвращает его
+const app = express(); //записываем результат вызова функции express
 const bodyParser = require("body-parser").json; //body-parser извлекает всю часть тела входящего потока запросов и предоставляет его на req.body
-
-/*let users = [
-  { "id": 1, "name": "firstTestUser" },
-  { "id": 2, "name": "firstTestUser2"}
-];*/
-let users = [];
+const MongoClient = require("mongodb").MongoClient; //класс
+const objectId = require("mongodb").ObjectID;
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+ 
 app.use(bodyParser());
 
-// returns a list of users
 app.get('/users', function(req, res) {
+  const collection = req.app.locals.collection;
+  collection.find({}).toArray(function(err,users){
+    if(err){
+      console.log(err);
+  }
   res.send(users);
+  });
 });
 
 app.get('/users/:id', function(req, res) {
-  const id = req.params.id;
-  const user = users.find(function(el){
-    return el.id == id; 
-  });
-  
-  if (!user) {
-    return res.status(404).end();
-  }
+  const collection = req.app.locals.collection;
+  const id = new objectId(req.params.id);
 
-  res.send(user);
-});
+  collection.findOne({_id:id},function(err,user){
+    if(err){
+      console.log(err);
+    }
+
+    if (!user) {
+      return res.status(404).end();
+    }
+    res.send(user);
+    });
+ });
+  
+  
 
 app.delete('/users/:id',function(req,res) {
-  const id = req.params.id;
-  users = users.filter(function(el) {
-    return el.id != id;
-  });          // если есть, то удаляет
-  return res.status(204).end();
+
+  const collection = req.app.locals.collection;
+  const id = new objectId(req.params.id);
+  
+  collection.deleteOne({_id:id},function(err, result){
+
+    if (result.deletedCount !== 1) {
+      return res.status(400).end();
+    }
+
+    res.status(204).end();
+  });
 });
+  
 
 app.put('/users/:id', function(req,res){
-  const id = req.params.id;
-  const user = users.find(function(el){
-      return el.id == id;
-  });
 
-  if(!user) {
-    return res.status(400).end();
-  }
-
-  user.name = req.body.name;
-
-  res.send(user);
+  const user = {
+    name: req.body.name,
+    age: req.body.age,
+  };
+  const collection = req.app.locals.collection;
+  const id = new objectId(req.params.id);
+  collection.findOneAndUpdate(
+    {_id: id}, 
+    { $set: user},
+    { returnOriginal: false },
+    function(err, result){
+      console.log(result);
+      res.send(result.value);
+    });
+   
 });
 
 app.post('/users', function(req,res){
-    
-  if(!req.body) {
-    return res.sendStatus(400);
-  }
-
-  let arrLength = users.length;
-  let idUser = arrLength + 1;
- /* let userName = {
-    name: req.body.name
-  };*/
+  if(!req.body) return res.sendStatus(400);
 
   let user = {
-    id: idUser,
-    name: req.body.name
+    name: req.body.name,
+    age: req.body.age,
   };
-//  user.name = nameUser; 
-  users.push(user);
 
-   res.send(user)
+  const collection = req.app.locals.collection;
+  collection.insertOne(user,function(err,result){
+    if(err){
+      return console.log(err);
+    }
+    res.send(user);
+  });
+
 });
 
-
-
-
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+mongoClient.connect(function(err,client){  //подключаемся к бд
+  if(err){
+      console.log(err);
+  }
+  app.locals.collection = client.db("users_db").collection("users"); //получаем ссылку на коллекцию в локальную переменную
+  dbClient = client; //сохраняем подключение в переменную
+  app.listen(3000, function(){
+      console.log("Слушаем порт 3000");
+  });
 });
+
 
 
