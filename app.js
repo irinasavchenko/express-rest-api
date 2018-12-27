eebfa39edf9ccb9289564bd231bc22901f04350a
Irina Savchenko <issavchenko.2011@gmail.com>
2018-12-27 22:08:16 +0200

    Switch to Mongoose

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const express = require('express'); //принимает имя модуля и возвращает его
-const app = express(); //записываем результат вызова функции express
+const express = require("express");
+const app = express(); // результат вызова функции express
 const bodyParser = require("body-parser").json; //body-parser извлекает всю часть тела входящего потока запросов и предоставляет его на req.body
-const MongoClient = require("mongodb").MongoClient; //класс
-const objectId = require("mongodb").ObjectID;
-const url = "mongodb://localhost:27017/";
-const mongoClient = new MongoClient(url, { useNewUrlParser: true });
- 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userScheme = new Schema ({
+  name:  String,
+  age: Number
+}, {
+  versionKey: false
+});
+const User = mongoose.model("User", userScheme); //создаем модель пользователя с названием User
+
 app.use(bodyParser());
 
 app.get('/users', function(req, res) {
-  const collection = req.app.locals.collection;
-  collection.find({}).toArray(function(err,users){
+  User.find({}, function(err, users) {
     if(err){
       console.log(err);
-  }
-  res.send(users);
+    }
+    res.send(users);
   });
 });
 
 app.get('/users/:id', function(req, res) {
-  const collection = req.app.locals.collection;
-  const id = new objectId(req.params.id);
 
-  collection.findOne({_id:id},function(err,user){
+  const id = req.params.id;
+
+  User.findOne({_id: id}, function(err, user){
     if(err){
       console.log(err);
     }
@@ -30,75 +34,63 @@ app.get('/users/:id', function(req, res) {
     if (!user) {
       return res.status(404).end();
     }
+
     res.send(user);
     });
  });
-  
-  
 
-app.delete('/users/:id',function(req,res) {
+app.delete('/users/:id', function(req, res) {
 
-  const collection = req.app.locals.collection;
-  const id = new objectId(req.params.id);
-  
-  collection.deleteOne({_id:id},function(err, result){
+  const id = req.params.id;
 
-    if (result.deletedCount !== 1) {
+  User.findByIdAndDelete(id, function(err, result) {
+    if (err) {
       return res.status(400).end();
     }
 
     res.status(204).end();
   });
 });
-  
 
-app.put('/users/:id', function(req,res){
+app.put('/users/:id', function(req,res) {
 
-  const user = {
-    name: req.body.name,
-    age: req.body.age,
-  };
-  const collection = req.app.locals.collection;
-  const id = new objectId(req.params.id);
-  collection.findOneAndUpdate(
-    {_id: id}, 
-    { $set: user},
-    { returnOriginal: false },
-    function(err, result){
-      console.log(result);
-      res.send(result.value);
-    });
-   
+  const userName = req.body.name;
+  const userAge = req.body.age;
+  const id = req.params.id;
+
+  User.findOneAndUpdate({_id: id}, {name: userName, age: userAge}, {new: true}, function(err, user) {
+    if(err) {
+      return console.log(err);
+    }
+
+    res.send(user);
+  });
 });
 
 app.post('/users', function(req,res){
-  if(!req.body) return res.sendStatus(400);
 
-  let user = {
-    name: req.body.name,
-    age: req.body.age,
-  };
+  if(!req.body) {
+    return res.sendStatus(400);
+  }
 
-  const collection = req.app.locals.collection;
-  collection.insertOne(user,function(err,result){
+  const userName = req.body.name;
+  const userAge = req.body.age;
+  const newUser = new User({name: userName, age: userAge});
+
+  newUser.save(function(err, result){
     if(err){
       return console.log(err);
     }
-    res.send(user);
+    res.send(newUser);
   });
-
 });
 
-mongoClient.connect(function(err,client){  //подключаемся к бд
-  if(err){
+mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true }, function(err){  //подключаемся к бд
+  if (err) {
       console.log(err);
   }
-  app.locals.collection = client.db("users_db").collection("users"); //получаем ссылку на коллекцию в локальную переменную
-  dbClient = client; //сохраняем подключение в переменную
+
   app.listen(3000, function(){
       console.log("Слушаем порт 3000");
   });
 });
-
-
-
