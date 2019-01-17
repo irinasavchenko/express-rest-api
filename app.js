ba63e1b59c03e79f633a9c4e98f88d36cd59fefa
Irina Savchenko <issavchenko.2011@gmail.com>
2019-01-17 11:54:48 +0200

    Added friend-requests

--- app.js
+++ app.js
@@ -3,15 +3,135 @@ const app = express(); // результат вызова функции express
 const bodyParser = require("body-parser").json; //body-parser извлекает всю часть тела входящего потока запросов и предоставляет его на req.body
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const PENDING_FRIEND_REQUEST = 'pending';
+const ACCEPTED_FRIEND_REQUEST = 'accepted';
+
 const userScheme = new Schema ({
   name:  String,
-  age: Number
+  age: Number,
+  friends: Array
 }, {
   versionKey: false
 });
+
 const User = mongoose.model("User", userScheme); //создаем модель пользователя с названием User
 
+const friendRequestScheme = new Schema({  //создаем тему запроса
+  sender_id: Schema.Types.ObjectId,
+  receiver_id: Schema.Types.ObjectId,
+  status: {
+    type: String,
+    default: PENDING_FRIEND_REQUEST,
+  }
+},
+ {
+  versionKey: false
+});
+
+const FriendRequest = mongoose.model("FriendRequest", friendRequestScheme); //создаем модель запроса
+
 app.use(bodyParser());
+
+app.get('/friend-requests', function(req, res) {
+  FriendRequest.find({}, function(err, requests) {
+    if(err){
+      console.log(err);
+    }
+    res.send(requests);
+  });
+});
+
+app.get('/friend-requests/:id', function(req, res){
+  const id = req.params.id;
+  FriendRequest.find({_id: id}, function(err, requests){
+    if(err) {
+      console.log(err);
+    }
+    res.send(requests);
+  })
+})
+
+app.post('/friend-requests', function(req,res){
+
+  if(!req.body) {
+    return res.sendStatus(400);
+  }
+
+  const sender_id = req.body.sender_id;
+  const receiver_id = req.body.receiver_id;
+
+  const friendRequest = new FriendRequest({
+    sender_id,
+    receiver_id,
+  });
+
+  friendRequest.save(function(err, result){
+    if(err){
+      return console.log(err);
+    }
+    res.send(friendRequest);
+  });
+});
+
+app.put('/friend-requests/:id', function(req, res) {
+
+  const receivedStatus = req.body.status;
+
+  FriendRequest.findOneAndUpdate({ _id: req.params.id }, { status: receivedStatus }, { new: true }, function(err, friendRequest) {
+    if (friendRequest.status === ACCEPTED_FRIEND_REQUEST ) {
+      User.find({ _id:  friendRequest.sender_id }, function (err, [user]) {
+
+        if(err) {
+          console.log(err);
+        }
+
+       user.friends.push(friendRequest.receiver_id);
+
+        user.save(function (err, result) {
+          if(err) {
+            console.log(err);
+          }
+        });
+      });
+
+      User.find({ _id:  friendRequest.receiver_id }, function (err, [user]) {
+
+        if(err) {
+          console.log(err);
+        }
+
+        user.friends.push(friendRequest.sender_id);
+
+        user.save(function (err, result) {
+          if(err) {
+            console.log(err);
+          }
+        });
+
+      });
+      res.send(friendRequest);
+    } else {
+      res.status(400).send("Response was rejected");
+    }
+  });
+
+  });
+
+  app.delete('/friend-requests/:id', function(req,res){
+    let id = req.params.id;
+    FriendRequest.findByIdAndDelete({_id: id}, function(err, result){
+      if(err){
+        console.log(err);
+      }
+      res.sendStatus(204);
+    });
+  });
+/**
+ *
+ * USERS
+ *
+ */
 
 app.get('/users', function(req, res) {
   User.find({}, function(err, users) {
@@ -39,13 +159,15 @@ app.get('/users/:id', function(req, res) {
     });
  });
 
+
 app.delete('/users/:id', function(req, res) {
 
   const id = req.params.id;
 
   User.findByIdAndDelete(id, function(err, result) {
+
     if (err) {
-      return res.status(400).end();
+      return res.status(500).end();
     }
 
     res.status(204).end();
@@ -75,13 +197,15 @@ app.post('/users', function(req,res){
 
   const userName = req.body.name;
   const userAge = req.body.age;
-  const newUser = new User({name: userName, age: userAge});
+  const userFriends = req.body.friends;
+
+  const newUser = new User({name: userName, age: userAge, friends: userFriends});
 
   newUser.save(function(err, result){
     if(err){
       return console.log(err);
     }
-    res.send(newUser);
+    res.status(201).send(newUser);
   });
 });
 
